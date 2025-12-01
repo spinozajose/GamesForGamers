5180c72cdd0d8d2744eef3ad2f42d99b67b1c396
Jose Espinoza <josi.espinoza@duocuc.cl>
2025-12-01 05:57:48 -0300

    configuracion de axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
+
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:8080/api'
+    // FORZAMOS la IP Pública de AWS directamente aquí
+    // Esto garantiza que React no intente conectarse a localhost nunca más.
+    baseURL: 'http://34.239.245.32:8080/api' 
 });
+
 export default clienteAxios;
